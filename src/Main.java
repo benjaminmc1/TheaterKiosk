ed85ac22bff4800be596dd574f6116a97c4e2bc9
Benjamin <bmckee566@gmail.com>
2023-06-13 17:04:59 -0400

    Task finish

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+
+        System.out.println("Enter your age: ");
+        int age = in.nextInt();
+
+        if(age == 21) {
+            System.out.println("You will receive a wrist band.");
+        } else if(age < 21) {
+            System.out.println("You do not get a wrist band.");
+        }
     }
 }
